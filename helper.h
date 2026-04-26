3f40c4feb1ceaa259436f01a85c0a895ce887ef4
Oct8Hacker <Aryan.Khadgi@iiitb.ac.in>
2026-04-26 14:33:03 +0530

    Remove client, server, helper, login, and necessary files; refactor project structure for improved organization

--- helper.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "necessary.h"
-int is_c_file(const char *filename);
-bool check_files(char *file_name);
-void send_data(char *file_name, int sd);
-bool rcv_data(char *file_name, int nsd, bool state);
-int disable_nagle(int fd);
--- server.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "necessary.h"
-#include "helper.h"
-void init_connection(int *sd);
